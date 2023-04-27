b068944ee92b3243583950e36b2e016081d7ce0a
Mavis <mavis.murdock@tufts.edu>
2023-04-27 15:57:22 -0400

    Use wait_for_message to get user input

--- src/shared_control.py
+++ src/shared_control.py
@@ -7,22 +7,22 @@ from geometry_msgs.msg import Twist
 if __name__ == "__main__":
     rospy.init_node("teleop_test", anonymous=True)
 
-    v = Twist()
-    v.linear.x = 0.1
+    # v = Twist()
+    # v.linear.x = 0.1
 
     arm = armpy.gen2_teleop.Gen2Teleop(ns="/j2s7s300_driver")
     # arm.set_velocity(v)
 
-    action=teleop_lib.plugins.user_cmd.User_Action() # A plugin that gets the user's velocity command
-    vel = action.listen() # this is always 0 for some reason
-    print(vel.twist)
-    arm.set_velocity(vel.twist)
-
-
+    action = teleop_lib.plugins.user_cmd.User_Action() # A plugin that gets the user's velocity command
+    # action.listen() # this is always 0 for some reason
+ 
+    # arm.set_velocity(vel.twist)
 
     try:
         while not rospy.is_shutdown():
-            rospy.spin() 
+            action.listen() # when we get a Joy message
+            print(action.cmd)
+     
     except:
         pass
 
@@ -36,3 +36,8 @@ if __name__ == "__main__":
     # Merge the two actions
 
     # Perform resulting action
+
+# def not_zero(cmd):
+#     if cmd.twist.linear.x == 0 and cmd.twist.linear.y == 0 and cmd.twist.linear.z == 0 and cmd.twist.angular.x == 0 and cmd.twist.angular.y == 0 and cmd.twist.angular.z == 0:
+#         return False
+#     return True
