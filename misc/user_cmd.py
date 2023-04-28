d8e443b23ca0015af0ac54ba42d0f23c066eb733
Mavis <mavis.murdock@tufts.edu>
2023-04-28 11:23:25 -0400

    Get end eff position, convert user action to unit vector

--- misc/user_cmd.py
+++ misc/user_cmd.py
@@ -1,6 +1,5 @@
 # this is actually stored in teleop_lib/plugins. This is just a copy for safekeeping.
 
-
 #!/usr/bin/env python3
 import rospy
 from sensor_msgs.msg import Joy
@@ -9,35 +8,24 @@ import yaml
 import teleop_lib.input_profile
 import sensor_msgs.msg
 
+# Listens for Joy input and converts it to velocity commands
 class User_Action:
     def __init__(self):
         with open("/home/mavis/catkin_ws/src/teleop_lib/data/XYMode.yaml") as f:
             self.cfg = yaml.safe_load(f)
 
-
         self.joy = sensor_msgs.msg.Joy(axes=[0.3, -3], buttons=[1, 0])
         self.mode = teleop_lib.input_profile.build_profile(self.cfg)
         self.cmd = None
-        # rospy.init_node("listener_test", anonymous=True)
 
     def listen(self):
         rospy.loginfo("Listening")
-        # sub = rospy.Subscriber("joy", sensor_msgs.msg.Joy, self.joy_cb, queue_size=1)
-        # rospy.spin()
+
         self.input = rospy.wait_for_message("/joy", sensor_msgs.msg.Joy)
         self.cmd = self.mode.process_input(self.input)
-        # print(self.cmd)
-        return self.cmd
-
-    # def joy_cb(self, msg):
-    #     self.cmd = self.mode.process_input(msg)
-    #     print(self.cmd)
  
 def main():
-    
     action=User_Action()
-    # action.listen()
-    # print(action.cmd)
 
 if __name__ == "__main__":
     main()
--- src/shared_control.py
+++ src/shared_control.py
@@ -3,41 +3,40 @@ import rospy
 import armpy.gen2_teleop
 import teleop_lib.plugins.user_cmd
 from geometry_msgs.msg import Twist
+from geometry_msgs.msg import PoseStamped
+import numpy as np
 
 if __name__ == "__main__":
     rospy.init_node("teleop_test", anonymous=True)
 
-    # v = Twist()
-    # v.linear.x = 0.1
-
     arm = armpy.gen2_teleop.Gen2Teleop(ns="/j2s7s300_driver")
-    # arm.set_velocity(v)
 
     action = teleop_lib.plugins.user_cmd.User_Action() # A plugin that gets the user's velocity command
-    # action.listen() # this is always 0 for some reason
- 
-    # arm.set_velocity(vel.twist)
+
 
     try:
         while not rospy.is_shutdown():
             action.listen() # when we get a Joy message
-            print(action.cmd)
+            arm.set_velocity(action.cmd.twist)
+            position = rospy.wait_for_message('/j2s7s300_driver/out/tool_pose', PoseStamped)
+
+            user_action = np.array([action.cmd.twist.linear.x, action.cmd.twist.linear.y, action.cmd.twist.linear.z])
+            end_eff_pos = np.array([position.pose.position.x, position.pose.position.y, position.pose.position.z])
+            print(end_eff_pos)
+            print(user_action)
+
+            unit_vector = user_action / np.linalg.norm(user_action)
+            print('unit vector: ', unit_vector)
+
+            # Get user's action as a unit vector (u)
+
+            # Get robot's predicted action (a)
+
+            # Merge the two actions
+
+            # Perform resulting action
      
     except:
         pass
 
-    # Get user's action as a unit vector (u)
-
-    # Get robot's predicted action (a)
-        # use dummy action
-    # dummy_vel = Twist()
-    # dummy_vel.linear.x = 0.1
-
-    # Merge the two actions
-
-    # Perform resulting action
-
-# def not_zero(cmd):
-#     if cmd.twist.linear.x == 0 and cmd.twist.linear.y == 0 and cmd.twist.linear.z == 0 and cmd.twist.angular.x == 0 and cmd.twist.angular.y == 0 and cmd.twist.angular.z == 0:
-#         return False
-#     return True
+    
