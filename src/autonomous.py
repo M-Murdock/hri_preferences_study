cb23449760c781ce748acb84d9a6296bde114991
Mavis <mavis.murdock@tufts.edu>
2023-06-05 17:01:45 -0400

    Use joint positions for autonomous mode

--- src/autonomous.py
+++ src/autonomous.py
@@ -3,25 +3,21 @@
 import moveit_commander
 import rospy
 import sys
-from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Pose, PoseStamped
 import yaml
 import armpy.arm
 import traceback
+import pickle
+import os
 
 class Autonomous:
     def __init__(self):
         try:
             moveit_commander.roscpp_initialize(sys.argv)
             self.arm = armpy.arm.Arm()
-
-            self.goal_dict = None
-            GOAL_NAME = 'goal1'
-
-            with open('/home/mavis/catkin_ws/src/hri_preferences_study/config/goals.yaml', 'r') as file:
-                output = yaml.safe_load(file)
-                # read in goals from yaml file
-                self.goal_dict = output.get(GOAL_NAME) 
-
+            # to set the goals, move arm to desired position, then `rostopic echo /joint_states`:
+            self.goal = [0.5080172525610042, 2.3989328993659362, 3.810842904997834, 2.0779030778687164, 4.201798148814774, 3.874152103063415, 6.318032770043272]
+            # self.goal2 = [0.7520974644948796, 2.5134703978441735, 4.0290772384210305, 2.820384000825374, 5.131428855793169, 3.9699422790480394, -1.2558463689082726]
             self.move()
 
         except:
@@ -29,18 +25,22 @@ class Autonomous:
             pass
 
     def move(self):
-        # self.pos = self.arm.get_current_ee_pose()
-        # self.pos.pose.position.x += 0.1
-        self.pos = Pose()
-        self.pos.position.x = self.goal_dict.get('position')['x'] 
-        self.pos.position.y = self.goal_dict.get('position')['y']
-        self.pos.position.z = self.goal_dict.get('position')['z']
+        plan = self.arm.plan_pose(self.goal, is_joint_pos=True) # True if joint states. False if eef
+        if plan != None:
+            self.arm.move_robot(plan, wait=True)
+            self.save_trajectory("test_trajectory", plan)
 
-        self.pos.orientation.x = self.goal_dict.get('orientation')['x']
-        self.pos.orientation.y = self.goal_dict.get('orientation')['y']
-        self.pos.orientation.z = self.goal_dict.get('orientation')['z']
-        self.pos.orientation.w = self.goal_dict.get('orientation')['w']
+    def save_trajectory(self, name, plan):
+        filename = "trajectories/"+name+".pkl"
+        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), filename)
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        with open(path, "wb") as f:
+            pickle.dump(plan, f)
 
-        print(self.pos)
-        self.arm.move_to_ee_pose(self.pos) # Move to specified end effector position
-        
+    def load_trajectory(self, name):
+        filename = "trajectories/"+name+".pkl"
+        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), filename)
+        with open (path, "rb") as f:
+            plan = pickle.load(f)
+        return plan 
+ 
